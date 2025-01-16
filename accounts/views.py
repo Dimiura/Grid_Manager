f096be1563bc5a068d2e3bb104c649ffb641d60c
Dimiura <di.miura@hotmail.com>
2025-01-15 22:48:36 -0300

    fix-loginview

--- accounts/views.py
+++ accounts/views.py
@@ -9,7 +9,7 @@ def register_view (request):
         user_form = UserCreationForm(request.POST)
         if user_form.is_valid():
             user_form.save()
-            return redirect('content')
+            return redirect('login')
     else:
             user_form = UserCreationForm()
     return render(request, 'register.html', {'user_form': user_form}) 
